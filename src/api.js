c2cd888b53d33538336d6f797169ebfd7a3c43f5
JiaSephjo <jianajoseph6@gmail.com>
2025-09-25 01:09:59 +0800

    Update api.js baseURL and rebuild frontend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
-// src/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // ✅ Controlled by .env
+  baseURL: process.env.REACT_APP_API_BASE_URL
 });
 
 export default api;
